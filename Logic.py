38cfcd48d886d7b1c7393cdf1ee6ae9ef3033a04
doran34 <90012644+doran34@users.noreply.github.com>
2022-10-04 10:53:12 +1000

    Create Logic.py
    logic outline. added block class.

---
+++ Logic.py
@@ -0,0 +1,46 @@
+#includes stuff
+import numpy as np
+centre_x = 0
+centre_y = 0
+
+class Block:
+    def __init__(self, x, y, z, theta)
+    self.x = x 
+    self.y = y
+    self.z = z
+    self.theta = theta
+    self.r = np.sqrt((self.x-centre_x)^2+(self.y-centre_y)^2)
+
+    def update_Pos(self, x, y, z, theta):
+        #Update a block opjects current position.
+        #maybe update r. 
+        return 0 
+
+    def get_Pos(self):
+        # Get blocks current position.
+        return 0 
+
+
+
+
+
+
+
+
+
+
+def transform(x, y, z, theta):
+    return 0 
+
+
+
+#sample 
+
+
+
+# copy gripper code 
+
+
+
+
+
