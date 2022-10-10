531d347d93f396384ff1f0ca86a2e7f6b6372aa8
Jocelyn Hsu <jocelynhsu00@gmail.com>
2022-10-10 23:40:28 +1000

    Testing

--- catkin_ws/inverse_kinematics_pkg/src/inverse_kinematics.py
+++ catkin_ws/inverse_kinematics_pkg/src/inverse_kinematics.py
@@ -59,50 +59,19 @@ def inverse_kinematics(pose: Pose) -> JointState:
 
     # Convert angles for input to dynamixels
     theta_2 = (np.pi/2) - theta_2
-    theta_3 = -theta_3
 
     theta_list = [theta_1, theta_2, theta_3, theta_4]
 
     # Check limits
 
-    # Check if angles have been updated 
-    angles_updated = False
+    all_valid = True
 
     for theta in theta_list:
         # Check collision
         if theta < -np.deg2rad(150) or theta > np.deg2rad(150):
-            # Indicate the angles are being updated
-            angles_updated = True
-
-            # Try orienting gripper at 0 deg to table
-            alpha = 0
-            # First calculate position at end of link 3
-            px_3 = px_end - l4 * np.cos(alpha)
-            py_3 = py_end - l4 * np.sin(alpha)
-            pz_3 = pz_end # z pos doesnt change
-
-            c_theta_3 = (px_3 ** 2 + py_3 ** 2 - l2 ** 2 - l3 ** 2) / (2 * l2 * l3)
-
-            theta_1 = np.arctan2(px_end, pz_end) # Maybe needs to 90 - theta_1 
-            theta_3 = np.arctan2(c_theta_3, - np.sqrt(1 - c_theta_3 ** 2))
-            theta_2 = np.arctan2(py_3, px_3) - np.arctan2(l2 + l3 * np.cos(theta_3), l3 * np.sin(theta_3))
-            theta_4 = alpha - (theta_2 + theta_3)
-
-            # Update theta list
-            theta_list = theta_list = [theta_1, theta_2, theta_3, theta_4]
+            all_valid = False
             break
 
-    # Check updated solution
-    if angles_updated:
-        all_valid = True
-        # Check collision
-        for theta in theta_list:
-            if theta < -np.deg2rad(150) or theta > np.deg2rad(150):
-                # Updated solution is invalid
-                all_valid = False
-                break
-
-    # Only publish valid solution
     if all_valid:
         msg.position = [
             theta_1,
@@ -112,6 +81,66 @@ def inverse_kinematics(pose: Pose) -> JointState:
         ]
         rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
         pub.publish(msg)
+
+    else:
+        # Set to random pos
+        print("Setting to standard pos")
+        msg.position = [
+            0,
+            0,
+            1.5,
+            0
+        ]
+        rospy.loginfo('Invalid pose, setting to random pos')
+        pub.publish(msg)
+
+    # # Check if angles have been updated 
+    # angles_updated = False
+
+    # for theta in theta_list:
+    #     # Check collision
+    #     if theta < -np.deg2rad(150) or theta > np.deg2rad(150):
+    #         # Indicate the angles are being updated
+    #         angles_updated = True
+
+    #         # Try orienting gripper at 0 deg to table
+    #         alpha = 0
+    #         # First calculate position at end of link 3
+    #         px_3 = px_end - l4 * np.cos(alpha)
+    #         py_3 = py_end - l4 * np.sin(alpha)
+    #         pz_3 = pz_end # z pos doesnt change
+
+    #         c_theta_3 = (px_3 ** 2 + py_3 ** 2 - l2 ** 2 - l3 ** 2) / (2 * l2 * l3)
+
+    #         theta_1 = np.arctan2(px_end, pz_end) # Maybe needs to 90 - theta_1 
+    #         theta_3 = np.arctan2(c_theta_3, - np.sqrt(1 - c_theta_3 ** 2))
+    #         theta_2 = np.arctan2(py_3, px_3) - np.arctan2(l2 + l3 * np.cos(theta_3), l3 * np.sin(theta_3))
+    #         theta_4 = alpha - (theta_2 + theta_3)
+
+    #         # Update theta list
+    #         theta_list = theta_list = [theta_1, theta_2, theta_3, theta_4]
+    #         break
+
+    # # Check updated solution
+    # if angles_updated:
+    #     all_valid = True
+    #     # Check collision
+    #     for theta in theta_list:
+    #         if theta < -np.deg2rad(150) or theta > np.deg2rad(150):
+    #             # Updated solution is invalid
+    #             all_valid = False
+    #             break
+
+    # # Only publish valid solution
+    # if all_valid:
+    #     msg.position = [
+    #         theta_1,
+    #         theta_2,
+    #         theta_3,
+    #         theta_4
+    #     ]
+    #     rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
+    #     pub.publish(msg)
 
 
 #     # Check if theta 4 is valid, change phi if not (alternates between 0 and 90, can set to arbitrary value later)
