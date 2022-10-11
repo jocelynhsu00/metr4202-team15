30c2a1ba033bc79a8b27ba24341ecea030543f5a
Jocelyn Hsu <71418349+jocelynhsu00@users.noreply.github.com>
2022-10-11 15:45:56 +1000

    Add files via upload

--- catkin_ws/inverse_kinematics_pkg/src/inverse_kinematics.py
+++ catkin_ws/inverse_kinematics_pkg/src/inverse_kinematics.py
@@ -26,80 +26,61 @@ def inverse_kinematics(pose: Pose) -> JointState:
         name=['joint_1', 'joint_2', 'joint_3', 'joint_4']
     )
     
-    # msg.position = [
-    #     -np.deg2rad(30),
-    #     np.deg2rad(30),
-    #     np.deg2rad(30),
-    #     -np.deg2rad(30)
-    # ]
-    # rospy.loginfo('Test 30 deg')
-    # pub.publish(msg)
     # End eff posiiton
     px_end = pose.position.x #end_eff[0]
     py_end = pose.position.y #end_eff[1]
     pz_end = pose.position.z #nd_eff[2]
 
-    print(px_end, py_end, pz_end)
+    psi = - np.pi / 2 # Gripper always angled at - 90 deg relative to ground, can be changed if needed
 
-    # Ensure no division by 0
-    if px_end != 0 and py_end != 0:
-        print('non-zero end eff')
+    # mm link lengths (from cad)
+    l1 = 53 # link_length[0] (not used)
+    l2 = 117.5 #link_lengths[1]
+    l3 = 95 #link_lengths[2]
+    l4 = 85 #link_lengths[3]
 
-        psi = - np.pi / 2 # Gripper always angled at - 90 deg relative to ground, can be changed if needed
+    # Get position of arm at theta 4
+    px_4 = px_end - l4 * np.cos(psi)
+    py_4 = py_end - l4 * np.sin(psi)
+    pz_4 = pz_end # Stays the same, robot is rigid in this position
 
-        # mm link lengths (from cad)
-        l1 = 53 # link_length[0] (not used)
-        l2 = 117.5 #link_lengths[1]
-        l3 = 95 #link_lengths[2]
-        l4 = 85 #link_lengths[3]
+    # Consider 2dof arm
+    theta_3 = np.pi - np.arccos((px_4**2 + py_4**2 - (l2**2 + l3**2))/(-2*l2*l3))
+    # theta_2 = np.arctan(py_4/px_4) - np.arctan((l3*np.sin(theta_3))/(l2+l3*np.cos(theta_3)))
+    theta_2 = np.arctan(py_4/px_4) - np.arcsin((l2*np.sin(np.arccos((px_4**2 + py_4**2 - (l2**2 + l3**2))/(-2*l2*l3))))/np.sqrt(px_4**2 + py_4**2))
 
-        # Get position of arm at theta 4
-        px_4 = px_end - l4 * np.cos(psi)
-        py_4 = py_end 
-        pz_4 = pz_end - l4 * np.sin(psi)
+    theta_4 = 0 # psi - (theta_2 + theta_3)
 
-        # Consider 2dof arm
-        theta_3 = np.arccos((px_4**2 + pz_4**2 - (l2**2 + l3**2))/(2*l2*l3))
-        theta_2 = np.arctan(pz_4/px_4) - np.arctan((l3*np.sin(theta_3))/(l2+l3*np.cos(theta_3)))
+    # Update angles for elbow up solution
+    theta_3 = - theta_3
+    theta_2 = - theta_2
 
-        # Update angles for elbow up solution
-        # theta_3 = - theta_3
-        theta_2 = np.pi/2 - theta_2
+    theta_1 = np.arctan(pz_end/px_end)
 
-        theta_4 = psi - (theta_2 + theta_3)
-        # theta_4 = - theta_4
+    # Check limits
+    theta_list = [theta_1, theta_2, theta_3, theta_4]
+    print("Calculated thetas:" , theta_list)
 
-        theta_1 = np.arctan(py_end/px_end)
-        theta_1 = - theta_1
+    # Assume all valid
+    all_valid = True
 
-        # Check limits
-        theta_list = [theta_1, theta_2, theta_3, theta_4]
-        print(theta_list)
+    for theta in theta_list:
+        # Check for invalid
+        if theta < -1.5 or theta > 1.5:
+            print("Invalid theta", theta)
+            # Invalid
+            all_valid = False
+            break
 
-        # Assume all valid
-        all_valid = True
-
-        for theta in theta_list:
-            # Check for invalid
-            if theta <= -1.5 or theta >= 1.5:
-                # Invalid
-                all_valid = False
-                break
-
-        # Only publish if angles are valid
-        if all_valid:
-            msg.position = theta_list
-            print("VALID:", theta_list)
-            rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
-            pub.publish(msg)
-        else:
-            msg.position = [0, 0, 0, 0]
-            rospy.loginfo('use 0 config')
-            pub.publish(msg)
+    # Only publish if angles are valid
+    if all_valid:
+        msg.position = theta_list
+        rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
+        pub.publish(msg)
 
     else:
         msg.position = [0, 0, 0, 0]
-        rospy.loginfo('use 0 config')
+        rospy.loginfo("Use zero config")
         pub.publish(msg)
 
 
