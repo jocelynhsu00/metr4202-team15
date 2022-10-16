2c2ff67cb69195a8770070bfd69c7acaffd6e341
Jocelyn Hsu <71418349+jocelynhsu00@users.noreply.github.com>
2022-10-16 11:41:50 +1000

    Debugging IK 0 deg gripper pos

--- catkin_ws/inverse_kinematics_pkg/src/inverse_kinematics.py
+++ catkin_ws/inverse_kinematics_pkg/src/inverse_kinematics.py
@@ -40,7 +40,14 @@ def inverse_kinematics(pose: Pose) -> JointState:
     z_end = pose.position.z 
 
     # Prevent self collision and hitting ground
-    if x_end_abs > 50 and y_end > -70:
+    if x_end_abs > 100 and y_end > -45:
+        print("Passed floor collision and env collision check")
+
+        # mm link lengths (from cad)
+        l1 = 75 # link_length[0] (used for horizontal grab)
+        l2 = 117.5 #link_lengths[1]
+        l3 = 95 #link_lengths[2]
+        l4 = 85 #link_lengths[3]
 
         # Check reachable within workspace
         # Gripper at 90 deg
@@ -48,6 +55,8 @@ def inverse_kinematics(pose: Pose) -> JointState:
 
         # Gripper at 0 deg
         max_length_gripper_0 = l2 + l4 + np.sqrt(l3**2 - l1**2)
+
+        print("Max lengths", max_length_gripper_90, max_length_gripper_0)
 
         gripper_90_valid = False
 
@@ -58,6 +67,8 @@ def inverse_kinematics(pose: Pose) -> JointState:
 
         if x_end_abs < max_length_gripper_90:
             gripper_90_valid = True
+
+        print("Check max:", gripper_0_valid, gripper_90_valid)
 
         # Try gripper 90 deg sln first
         if gripper_90_valid:
@@ -75,18 +86,14 @@ def inverse_kinematics(pose: Pose) -> JointState:
             theta_4 = -psi - theta_2 - theta_3
             theta_1 = np.arctan(z_4/x_4)
 
-            theta_2 = np.arctan2(y_4, x_4) - np.arctan2(l3*np.sin(theta_3), l2 + l3*np.cos(theta_3))
-            theta_4 = -psi - theta_2 - theta_3
-            theta_1 = np.arctan(z_4/ x_4)
-
-            # Convert to dynamixel input 
+            # Convert to dynamixel input TODO: check if any of the dynamixel values need to be negated/flipped (so far seems like 4 needs to be)
             theta_3 = -theta_3
 
             theta_2 = np.pi/2 - theta_2
             if x_4 < 0:
                 theta_2 = - theta_2
             
-            # Negating for dynamixels
+            # Negating
             theta_4 = -theta_4
             theta_3 = -theta_3
             theta_2 = -theta_2
@@ -98,6 +105,7 @@ def inverse_kinematics(pose: Pose) -> JointState:
                 theta_4 = -theta_4
                 theta_3 = -theta_3
                 theta_2 = -theta_2
+
 
             theta_list  = [theta_1, theta_2, theta_3, theta_4]
             # Check limits
@@ -117,6 +125,7 @@ def inverse_kinematics(pose: Pose) -> JointState:
                 msg.position = theta_list
 
             elif not all_valid and gripper_0_valid:
+                print("Trying 0")
                 # Try gripper 0 position
                 psi = 0
 
@@ -136,13 +145,13 @@ def inverse_kinematics(pose: Pose) -> JointState:
                 theta_4 = -psi - theta_2 - theta_3
                 theta_1 = np.arctan(z_4/x_4)
 
-                # Convert to dynamixel input 
+                # Convert to dynamixel input TODO: check if any of the dynamixel values need to be negated/flipped (so far seems like 4 needs to be)
                 theta_3 = -theta_3
                 theta_2 = np.pi/2 - theta_2
                 if x_4 < 0:
                     theta_2 = - theta_2
 
-                # Negating for dynamixels
+                # Negating
                 theta_4 = -theta_4
                 theta_3 = -theta_3
                 theta_2 = -theta_2
@@ -157,6 +166,7 @@ def inverse_kinematics(pose: Pose) -> JointState:
 
                 theta_list  = [theta_1, theta_2, theta_3, theta_4]
                 # Check limits
+                # TODO: COLLISION AVOIDANCE
                 all_valid = True
                 for index, theta in enumerate(theta_list):
                     # Check dynamixel limits (want to avoid going over -90, 90 deg as it damages the wiring)
@@ -177,6 +187,67 @@ def inverse_kinematics(pose: Pose) -> JointState:
                 
                 if all_valid:
                     msg.position = theta_list
+
+                deg = []
+                for theta in theta_list:
+                    deg.append(np.rad2deg(theta))
+                print(deg)
+                rospy.loginfo(f'Got desired pose\n[\n\tpos:\n{pose.position}\nrot:\n{pose.orientation}\n]')
+                pub.publish(msg)
+
+        elif gripper_0_valid:
+            psi = 0
+
+            # Find pos at dynamixel 4
+            x_4 = x_end_abs
+            y_4 = y_end + l4
+            z_4 = z_end
+
+            # Elbow up
+            c_theta_3 = (x_4**2+y_4**2-l2**2-l3**2)/(2*l2*l3)
+            theta_3 = np.arctan2(-np.sqrt(1-c_theta_3), c_theta_3)
+            theta_2 = np.arctan2(y_4, x_4) - np.arctan2(l3*np.sin(theta_3), l2 + l3*np.cos(theta_3))
+            theta_4 = -psi - theta_2 - theta_3
+            theta_1 = np.arctan(z_4/x_4)
+            print(theta_1, theta_2, theta_3, theta_4)
+
+            # Convert to dynamixel input TODO: check if any of the dynamixel values need to be negated/flipped (so far seems like 4 needs to be)
+            theta_3 = -theta_3
+
+            theta_2 = np.pi/2 - theta_2
+            if x_4 < 0:
+                theta_2 = - theta_2
+            
+            # Negating
+            theta_4 = -theta_4
+            theta_3 = -theta_3
+            theta_2 = -theta_2
+            theta_1 = -theta_1
+
+            # Negate if -ve x
+            if x_end < 0:
+                # Negating
+                theta_4 = -theta_4
+                theta_3 = -theta_3
+                theta_2 = -theta_2
+
+
+            theta_list  = [theta_1, theta_2, theta_3, theta_4]
+            # Check limits
+            all_valid = True
+            for index, theta in enumerate(theta_list):
+                # Check dynamixel limits (want to avoid going over -90, 90 deg as it damages the wiring)
+                if index != 3:
+                    if theta < -np.deg2rad(140) or theta > np.deg2rad(140) or math.isnan(theta):
+                        all_valid = False
+                        break
+                else:
+                    if theta < -np.deg2rad(110) or theta > np.deg2rad(110) or math.isnan(theta):
+                        all_valid = False
+                        break
+            
+            if all_valid:
+                msg.position = theta_list
 
             deg = []
             for theta in theta_list:
@@ -205,7 +276,8 @@ def main():
     # Initialise node with any node name
     rospy.init_node('joint_states')
 
-    # Stops Python from exiting and executes callbacks
+    # You spin me right round baby, right round...
+    # Just stops Python from exiting and executes callbacks
     rospy.spin()
 
 if __name__ == '__main__':
