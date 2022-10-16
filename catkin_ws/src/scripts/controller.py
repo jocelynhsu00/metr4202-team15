7c8212fe9e7a077fe2fbd1ee2ecd5d40896f5a6f
doran34 <90012644+doran34@users.noreply.github.com>
2022-10-16 12:04:36 +1000

    added some states

---
+++ catkin_ws/src/scripts/controller.py
@@ -0,0 +1,19 @@
+while(1):
+    state = 0
+
+    if state == 0 :
+        #initialise 
+        
+        #find blocks (not sure how to do this should be in max's code)
+
+        #create block classes 
+                
+
+    if state == 1 :
+
+    if state == 2 :
+        #Grab and move 
+
+    if state == 3 :
+        #reset 
+
