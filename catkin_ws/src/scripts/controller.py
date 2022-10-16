c1336dfb29c73a0feae13d94ffe43ade9dc99c24
doran34 <90012644+doran34@users.noreply.github.com>
2022-10-16 12:41:38 +1000

    did some things

--- catkin_ws/src/scripts/controller.py
+++ catkin_ws/src/scripts/controller.py
@@ -1,8 +1,7 @@
-from Logic.py import Block 
+import Logic.py as L
 
-
+state = 0
 while(1):
-    state = 0
 
     if state == 0 :
         #initialise 
@@ -10,21 +9,34 @@ while(1):
         #find blocks (not sure how to do this should be in max's code)
 
         #create block classes 
-        blue = Block(x, y, z)
-        green = Block(x, y, z)
-        red = Block(x, y, z)
-        yellow = Block(x, y, z)
+        blue = L.Block(x, y, z)
+        green = L.Block(x, y, z)
+        red = L.Block(x, y, z)
+        yellow = L.Block(x, y, z)
+
+        state += 1 
 
                 
 
     if state == 1 :
         #choose what block to pick up 
+        #Not sure how to do this
+
+        selected_block = green # or red or blue or what ever you chose 
+
+        state += 1 
 
 
     if state == 2 :
         #Grab and move 
+        selected_block.L.grab_box()
+
+        state += 1 
+
 
 
     if state == 3 :
         #reset 
+
+        state = 0
         
